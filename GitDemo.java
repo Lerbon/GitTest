a7a596072f49e721af889d5bd5737eba09bec778
York <386519091@qq.com>
2018-10-15 21:01:32 +0800

    冒泡排序

---
+++ GitDemo.java
@@ -0,0 +1,21 @@
+package com.atguigu.java.git;
+
+public class GitDemo {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld...");
+		int[] a = {12,3,5,65,7,98,54,1,43,10,42,99,11};
+		int temp = 0;
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length -1; j++) {
+				if (a[j] < a[i]) {
+					temp = a[j];
+					a[j] = a[i];
+					a[i] = temp;
+				}
+			}
+		}
+		for (int i : a) {
+			System.out.print(i + "  ");
+		}
+	}
+}
