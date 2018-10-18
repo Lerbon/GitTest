540bbe2a8dc8240d9d3b65182cb69cb8c965182c
York <386519091@qq.com>
2018-10-18 10:21:35 +0800

    删除武功

--- GitTest.java
+++ GitTest.java
@@ -7,19 +7,13 @@
 class GitTest
 {
 	public static void main (String[] args){
-		System.out.println("如来神掌!");
-		System.out.println("狮吼功!");
-		System.out.println("降龙十八掌!");
-		System.out.println("--------------------------------");
-		System.out.println("六脉神剑!");
 		system.out.println("姚老板真帅!");
-		System.out.println("蛤蟆神功!");
 		System.out.println("喊姚老板真帅的!报上名来,你语法都错了!");
 		System.out.println("姚老板眼睛真毒！")
-		System.out.println("你自己不会看是谁修改的吗？白眼儿~~~")
+		System.out.println("你自己不会看是谁修改的吗？白眼儿~~~");
                 System.out.println("抓住我我就让你嘿嘿嘿。。。。");
 		System.out.println("今天是个好日子！！！");
-		System.out.println("悄悄改改·····")
+		System.out.println("悄悄改改·····");
 
 		System.out.println("看你们一个个的,不是大小写错误就是漏分号;;;;;;;;;;");
 		System.out.println("----------Java学完了,main方法也不会写了");
